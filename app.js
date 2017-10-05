dd507c62d2e391f1534ad1d911a77bc5948d0eec
Spencer Hsieh <spencerhsieh@Spencers-MacBook-Pro.local>
2017-10-05 10:23:08 -0500

    port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 var bodyParser = require('body-parser');
 var request = require('request');
+var port = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -26,6 +27,6 @@ app.post('/', function (req, res) {
     res.send(200);
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!')
 });
