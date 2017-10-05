6ab893d5ac7814a80ff4b908de280371c1aa2fc7
Spencer Hsieh <spencerhsieh@Spencers-MacBook-Pro.local>
2017-10-05 15:53:58 -0500

    307

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 const express = require('express')
 const app = express()
-var bodyParser = require('body-parser');
-var request = require('request');
+// var bodyParser = require('body-parser');
+// var request = require('request');
 var port = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/api', function(req, res) {
     res.send('Hello', 200);
 })
 
 app.post('/api/rethinkdev-developer-edition.na59.force.com/services/apexrest/giftHook', function (req, res) {
+    var url = 'https://rethinkdev-developer-edition.na59.force.com/services/apexrest/giftHook';
     var body = req.body;
 
     console.log(req);
 
-    request.post(
-        'https://rethinkdev-developer-edition.na59.force.com/services/apexrest/giftHook',
-        {
-            form: body
-        },
-        function(error, response, body) {
+    // request.post(
+    //     url,
+    //     {
+    //         form: body
+    //     },
+    //     function(error, response, body) {
+    //
+    //     }
+    // );
 
-        }
-    );
+    res.redirect(307, url);
 
     res.sendStatus(200);
 });
